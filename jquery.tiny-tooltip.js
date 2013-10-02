fd90bf4e1fb4293513ed627ccc44a1fc8c78ad44
poturaev <333@ffe.com>
2013-10-02 18:25:19 +0700

    events

--- jquery.tiny-tooltip.js
+++ jquery.tiny-tooltip.js
@@ -1,6 +1,6 @@
 // https://github.com/vearutop/jquery-tiny-tooltip
 ;(function($) {
-    var mouseX, mouseY, $window = $(window);
+    var mouseX, mouseY, $window = $(window), last;
 
     $(document).mousemove(function(e){
         mouseX = e.pageX;
@@ -12,6 +12,7 @@
             var b = $(this),
                 o = {
                     content: b.attr('title'),
+                    clickSwitch: 0,
                     showDelay: 300,
                     hideDelay: 500,
                     className: 'tooltip',
@@ -25,24 +26,45 @@
             $.extend(o, options);
             b.attr('title', '');
 
-            if (o.content instanceof jQuery) {
-                o.content = o.content.html();
-            }
-
-
             var tipOver, tipVisible, elementOver, timer, tipElement;
 
             tipElement = $('<div class="' + o.className
-                + ' ' + o.classHidden +  '" style="position: absolute;">' + o.content + '</div>');
+                + ' ' + o.classHidden +  '" style="position: absolute;"></div>');
+
             $('body').append(tipElement);
+            if (o.content instanceof jQuery) {
+                tipElement.append(o.content.detach());
+            }
+            else {
+                tipElement.html(o.content);
+            }
+
+
             var tipHeight = $(tipElement).height(),
                 tipWidth = $(tipElement).width();
 
             hide();
 
+            function clickToggle() {
+                if (tipVisible) {
+                    console.log('hide');
+                    tipOver = elementOver =0;
+                    hide();
+                }
+                else {
+                    console.log('show');
+                    tipOver = elementOver = 1;
+                    show();
+                }
+            }
+
             function show() {
                 if ((!tipOver && !elementOver) || tipVisible) {
                     return;
+                }
+
+                if (last) {
+                    last.hide();
                 }
 
                 var centerX = $window.width() / 2,
@@ -64,6 +86,8 @@
                 }
 
                 tipVisible = 1;
+
+                last = tipElement;
             }
 
             function hide() {
@@ -91,25 +115,30 @@
                 timer = setTimeout(hide, option(sHideDelay));
             }
 
-            tipElement.mouseover(function(){
-                tipOver = 1;
-                mouseEvent(show, o.showDelay);
-            });
+            if (o.clickSwitch) {
+                b.click(clickToggle);
+            }
+            else {
+                tipElement.mouseover(function(){
+                    tipOver = 1;
+                    mouseEvent(show, o.showDelay);
+                });
 
-            b.mouseover(function(){
-                elementOver = 1;
-                mouseEvent(show, o.showDelay);
-            });
+                b.mouseover(function(){
+                    elementOver = 1;
+                    mouseEvent(show, o.showDelay);
+                });
 
-            b.mouseout(function(){
-                elementOver = 0;
-                mouseEvent(hide, o.hideDelay);
-            });
+                b.mouseout(function(){
+                    elementOver = 0;
+                    mouseEvent(hide, o.hideDelay);
+                });
 
-            tipElement.mouseout(function(){
-                tipOver = 0;
-                mouseEvent(hide, o.hideDelay);
-            });
+                tipElement.mouseout(function(){
+                    tipOver = 0;
+                    mouseEvent(hide, o.hideDelay);
+                });
+            }
 
         });
 
